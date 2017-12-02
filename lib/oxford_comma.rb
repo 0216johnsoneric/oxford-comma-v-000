a14611e3651f14a2cd150686cb77f3f987d5d8da
Eric Johnson <0216johnsoneric@gmail.com>
2017-12-02 22:30:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   elsif array.size == 3
-    "and" array.length [-1] array.join(" , ")
+    array.join(" , ")
   else
   end
 end
